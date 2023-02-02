371599f59598816cb9267c0f4bf7ea2e9d9147d9
ian-carlos <ian.carlos@comlink.com.br>
2023-02-02 14:30:06 -0300

    Ainda faltam coisas na minha API pra poder ter uma boa interconnectividade de acessos

--- src/main/java/com/lanchonete/controller/BebidaController.java
+++ src/main/java/com/lanchonete/controller/BebidaController.java
@@ -27,49 +27,50 @@ import com.lanchonete.repository.BebidaRepository;
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RequestMapping("/bebidas")
 public class BebidaController {
-	@Autowired BebidaRepository bebidaRepository;
-	
-	@GetMapping
-	public List<Bebida> listBebida() {
-		return bebidaRepository.findAll();
-	}
-	
-	@GetMapping("/{id}")
-	public ResponseEntity<?> getOneBebida(@PathVariable(name="id")long id) {
-		Optional<Bebida> bebidaOptional = bebidaRepository.findById(id);
-		return bebidaOptional.<ResponseEntity<Object>>map(bebida -> ResponseEntity.status(HttpStatus.OK).body(bebida)).orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).body("Bebida not found"));
-	}
-	
-	
-	@PostMapping
-	@ResponseStatus(HttpStatus.CREATED)
-	public Bebida addBebida(@RequestBody @Valid Bebida bebidaDto) {
-		Bebida bebida= new Bebida();
-		BeanUtils.copyProperties(bebidaDto, bebida);
-		return bebidaRepository.save(bebida);
-	}
-	
-	@PutMapping("/{id}")
-	public ResponseEntity<?> update(@PathVariable(name="id")long id, @RequestBody @Valid Bebida bebida){
-		Optional<Bebida> bebidaOptional = bebidaRepository.findById(id);
-		if (!bebidaOptional.isPresent()) {
-			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Bebida not found");
-		}
-		
-		Bebida bebidaUpdt = new Bebida();
-		BeanUtils.copyProperties(bebida, bebidaUpdt);
-		bebidaUpdt.setId(bebidaOptional.get().getId());
-		
-		return ResponseEntity.status(HttpStatus.OK).body(bebidaRepository.save(bebidaUpdt));
-	}
-	
-	@DeleteMapping("/{id}")
-	public ResponseEntity<?> delete(@PathVariable(name="id")long id) {
-		Optional<Bebida> bebidaOptional = bebidaRepository.findById(id);
-		if (!bebidaOptional.isPresent()) {
-			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Bebida not found");
-		}
-		bebidaRepository.delete(bebidaOptional.get());
-		return ResponseEntity.status(HttpStatus.OK).body("Bebida deleted successfully");
-	}
+    @Autowired
+    BebidaRepository bebidaRepository;
+
+    @GetMapping
+    public List<Bebida> listBebida() {
+        return bebidaRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getOneBebida(@PathVariable(name = "id") long id) {
+        Optional<Bebida> bebidaOptional = bebidaRepository.findById(id);
+        return bebidaOptional.<ResponseEntity<Object>>map(bebida -> ResponseEntity.status(HttpStatus.OK).body(bebida)).orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).body("Bebida not found"));
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<?> addBebida(@RequestBody @Valid Bebida bebidaDto) {
+        Bebida bebida = new Bebida();
+        BeanUtils.copyProperties(bebidaDto, bebida);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(bebidaRepository.save(bebida));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> update(@PathVariable(name = "id") long id, @RequestBody @Valid Bebida bebida) {
+        Optional<Bebida> bebidaOptional = bebidaRepository.findById(id);
+        if (!bebidaOptional.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Bebida not found");
+        }
+
+        Bebida bebidaUpdt = new Bebida();
+        BeanUtils.copyProperties(bebida, bebidaUpdt);
+        bebidaUpdt.setId(bebidaOptional.get().getId());
+
+        return ResponseEntity.status(HttpStatus.OK).body(bebidaRepository.save(bebidaUpdt));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable(name = "id") long id) {
+        Optional<Bebida> bebidaOptional = bebidaRepository.findById(id);
+        if (!bebidaOptional.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Bebida not found");
+        }
+        bebidaRepository.delete(bebidaOptional.get());
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build(); //Legal
+    }
 }
